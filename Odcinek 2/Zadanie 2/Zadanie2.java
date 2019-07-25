f04c7d231072408f511d2e58604d9106baa1fbc2
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 15:20:58 +0200

    Create Zadanie2.java

---
+++ Odcinek 2/Zadanie 2/Zadanie2.java
@@ -0,0 +1,7 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        char letter = 'a';
+        System.out.println(++letter);
+        System.out.println(letter + 2039);
+    }
+}
