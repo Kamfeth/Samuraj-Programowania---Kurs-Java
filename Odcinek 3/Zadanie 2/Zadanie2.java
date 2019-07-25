dbc53de5a53a9440db6948a5ab3c9059e528922d
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 21:03:05 +0200

    Create Zadanie2.java

---
+++ Odcinek 3/Zadanie 2/Zadanie2.java
@@ -0,0 +1,7 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[] {11111, 111, 11111111};
+        for (int number : numbers)
+            System.out.println(number);
+    }
+}
