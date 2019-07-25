5852d60966f10899dce1760b2b5e03d12770e721
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 15:22:42 +0200

    Create Zadanie4.java

---
+++ Odcinek 2/Zadanie 4/Zadanie4.java
@@ -0,0 +1,8 @@
+public class Zadanie4 {
+    public static void main(String[] args) {
+        String result = "Wynikiem dodawania liczby 2 i 2 jest ";
+        int number = 2;
+        number += 2;
+        System.out.print(result + number);
+    }
+}
