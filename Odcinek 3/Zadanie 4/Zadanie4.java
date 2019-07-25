446e2d1a190643b3c9c5deb27468bbfd71b91d9d
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 21:03:52 +0200

    Create Zadanie4.java

---
+++ Odcinek 3/Zadanie 4/Zadanie4.java
@@ -0,0 +1,7 @@
+public class Zadanie4 {
+    public static void main(String[] args) {
+        int[] numbers = {11, 22, 33, 44, 55, 66, 77, 88, 99, 110};
+        for (int number : numbers)
+            System.out.print(number + " ");
+    }
+}
