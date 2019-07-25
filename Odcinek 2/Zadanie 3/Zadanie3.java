e2962049c2e4b578bd390c3294836cea91bf264c
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 15:21:58 +0200

    Create Zadanie3.java

---
+++ Odcinek 2/Zadanie 3/Zadanie3.java
@@ -0,0 +1,8 @@
+public class Zadanie3 {
+    public static void main(String[] args) {
+        double number = 10.123456789;
+        for (int i = 0; i < 10; i++)
+            number++;
+        System.out.print(number);
+    }
+}
