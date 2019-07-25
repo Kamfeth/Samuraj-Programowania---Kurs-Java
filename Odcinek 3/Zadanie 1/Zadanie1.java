2a444e323f920b9e93847a8136e7857d31c2bc57
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 21:02:41 +0200

    Create Zadanie1.java

---
+++ Odcinek 3/Zadanie 1/Zadanie1.java
@@ -0,0 +1,9 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+        String stars = "";
+        for (int i = 0; i < 10; i++) {
+            stars += '*';
+            System.out.println(stars);
+        }
+    }
+}
