5258af4e3d31d79e1d7626718b1c13c4f71033de
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-29 19:48:30 +0200

    Create Zadanie1.java

---
+++ Odcinek 4/Zadanie 1/Zadanie1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        if (isPrime(number) && number > 1 && number != 4)
+            System.out.print("Liczba " + number + " jest liczbą pierwszą.");
+        else
+            System.out.print("Liczba " + number + " nie jest liczbą pierwszą.");
+    }
+
+    static boolean isPrime(int number) {
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
