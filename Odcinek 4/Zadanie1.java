7246c936be214355d0619ae8031fc397867386ff
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-29 19:43:09 +0200

    Update Zadanie1.java

--- Odcinek 4/Zadanie1.java
+++ Odcinek 4/Zadanie1.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Zadanie1 {
     public static void main(String[] args) {
-        System.out.print("Proszę wprowadzić liczbę naturalną: ");
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
         if (isPrime(number) && number > 1 && number != 4)
