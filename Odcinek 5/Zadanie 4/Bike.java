9a9865b634ee7509d738a141a2f5641af256996e
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-08-07 16:16:38 +0200

    Create Bike.java

---
+++ Odcinek 5/Zadanie 4/Bike.java
@@ -0,0 +1,7 @@
+public class Bike {
+    SteeringWheel sw = new SteeringWheel();
+    Pedals pedals = new Pedals();
+    Chain chain = new Chain();
+    FrontWheel fw = new FrontWheel();
+    RearWheel rw = new RearWheel();
+}
