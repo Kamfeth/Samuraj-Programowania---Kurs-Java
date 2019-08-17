6299bcb107fb2f10b5690da3ef319af29c781b00
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-17 20:34:55 +0200

    Update Zadanie1.java

--- Odcinek 2/Zadanie 1/Zadanie1.java
+++ Odcinek 2/Zadanie 1/Zadanie1.java
@@ -2,6 +2,10 @@ public class Zadanie1 {
     public static void main(String[] args) {
         int z1 = 15;
         int z2 = 5;
-        System.out.print("15 + 5 = " + (z1 + z2) + "\n15 - 5 = " + (z1 - z2) + "\n15 * 5 = " + z1 * z2 + "\n15 / 5 = " + z1 / z2 + "\n15 % 5 = " + z1 % z2);
+        System.out.println("15 + 5 = " + (z1 + z2));
+        System.out.println("15 - 5 = " + (z1 - z2));
+        System.out.println("15 * 5 = " + z1 * z2);
+        System.out.println("15 / 5 = " + z1 / z2);
+        System.out.println("15 % 5 = " + z1 % z2);
     }
 }
