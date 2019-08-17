9c39cb1da64e7889f8e34db4e6feaba51f433164
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-17 20:35:22 +0200

    Update Zadanie2.java

--- Odcinek 2/Zadanie 2/Zadanie2.java
+++ Odcinek 2/Zadanie 2/Zadanie2.java
@@ -2,6 +2,6 @@ public class Zadanie2 {
     public static void main(String[] args) {
         char letter = 'a';
         System.out.println(++letter);
-        letter += 2039;
+        System.out.println(letter + 2039);
     }
 }
