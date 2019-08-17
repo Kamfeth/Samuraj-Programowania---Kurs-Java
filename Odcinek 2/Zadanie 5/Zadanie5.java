a7ddbb21a22fc360ba07e861fc27c2c6cfca03a1
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-17 20:27:18 +0200

    Update Zadanie5.java

--- Odcinek 2/Zadanie 5/Zadanie5.java
+++ Odcinek 2/Zadanie 5/Zadanie5.java
@@ -1,6 +1,5 @@
 public class Zadanie5 {
     public static void main(String[] args) {
-        System.out.println("Obwód koła jest równy " + 2 * Math.PI * 47);
-        System.out.println("Pole koła jest równe " + Math.PI * 47 * 47);
+        System.out.print("Obwód koła jest równy " + 2 * Math.PI * 47 + "\nPole koła jest równe " + Math.PI * 47 * 47);
     }
 }
