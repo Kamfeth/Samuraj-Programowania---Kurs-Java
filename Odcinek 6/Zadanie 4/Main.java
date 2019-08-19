d4617761997644b823b207ca5fb9623c70a2cb33
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-19 14:26:58 +0200

    Add files via upload

---
+++ Odcinek 6/Zadanie 4/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("21 + 37 = " + Mathematics.addition(21, 37));
+        System.out.println("13 + 42 = " + Mathematics.addition(13, 42));
+        System.out.println("64 - 31 = " + Mathematics.subtraction(64, 31));
+        System.out.println("134 - 81 = " + Mathematics.subtraction(134, 81));
+        System.out.println("256 * 2 = " + Mathematics.multiplication(256, 2));
+        System.out.println("57 * 6 = " + Mathematics.multiplication(57, 6));
+        System.out.println("1024 / 128 = " + Mathematics.division(1024, 128));
+        System.out.println("60 / 3 = " + Mathematics.division(60, 3));
+    }
+}
