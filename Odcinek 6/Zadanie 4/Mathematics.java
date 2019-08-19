934242ed3232aae1e9635982be2d3e62e0581798
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-19 14:26:41 +0200

    Create Mathematics.java

---
+++ Odcinek 6/Zadanie 4/Mathematics.java
@@ -0,0 +1,17 @@
+public class Mathematics {
+    public static int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public static int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public static int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public static int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
