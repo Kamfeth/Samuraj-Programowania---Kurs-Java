1995cae7da7c7090152a4700fea9166b362ad18d
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-09-16 19:51:09 +0200

    Create Zadanie2.java

---
+++ Odcinek 7/Zadanie 2/Zad2/Zadanie2.java
@@ -0,0 +1,14 @@
+package zad2;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        System.out.print("9! jest równe " + factorial(9));
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+}
